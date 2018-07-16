dce063b09f19bb842ac13e4e06b7e0e0c65cf61b
Lynn Smeria <ae@cephea.de>
2018-07-16 15:09:29 +0300

    Add common mixins
    Extracted some mixins from the default theme that should be useful for all themes.

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,14 +1,14 @@
 const test = require('ava');
 const fs = require('fs');
-const { generateHTML, readCV } = require('../src');
+const { generateHTML, parseCV } = require('../src');
 const { readPath, resolveTemplate, resolveYML } = require('../src/resolve');
 
 process.chdir(__dirname);
 
-const cv = readCV(readPath(resolveYML('cv.yml')));
+const cv = parseCV(readPath(resolveYML('cv.yml')));
 const themes = fs.readdirSync('../themes');
 
-themes.forEach(theme =>
+themes.filter(theme => theme !== 'common.pug').forEach(theme =>
   test(`correctly applies theme "${theme}"`, t => {
     const template = resolveTemplate(theme);
     t.snapshot(generateHTML(cv, template));
