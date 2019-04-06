a518937d1417a060ba362d2b37ebab53f2187d29
Lynn Smeria <ae@cephea.de>
2019-04-06 18:17:53 +0900

    Add localisation support

--- themes/default.jsx
+++ themes/default.jsx
@@ -1,118 +1,152 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import LocalizedStrings from 'react-localization';
 import ReactMarkdown from 'react-markdown';
 
-exports.default = props => (
-  <>
-    <Helmet>
-      <title>{props.profile.name}</title>
-      <link
-        rel="stylesheet"
-        href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-        crossorigin="anonymous"
-      />
-      <meta name="charset" content="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Helmet>
-    <main>
-      <Profile {...props.profile} social={props.social} />
-      {Object.keys(props).map(key =>
-        sections[key]
-          ? React.createElement(sections[key], { key, data: props[key] })
-          : null
-      )}
-    </main>
+exports.default = props => {
+  strings.setLanguage(props.language);
+  // const sections
+  return (
+    <>
+      <Helmet>
+        <title>{props.profile.name}</title>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+          crossorigin="anonymous"
+        />
+        <meta name="charset" content="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Helmet>
+      <main>
+        <Profile {...props.profile} social={props.social} />
+        {Object.keys(props).map(key =>
+          sections[key]
+            ? React.createElement(sections[key], { key, data: props[key] })
+            : null
+        )}
+      </main>
 
-    <style jsx global>{`
-      @import url('https://fonts.googleapis.com/css?family=Exo+2:200|Merriweather:300,700');
+      <style jsx global>{`
+        @import url('https://fonts.googleapis.com/css?family=Exo+2:200|Merriweather:300,700');
 
-      :root {
-        --background: white;
-        --text-color: hsl(0, 0%, 15%);
-        --theme-color: hsl(26, 100%, 50%);
-        --light-color: hsl(0, 0%, 60%);
-        --anchor-color: hsl(26, 100%, 25%);
-        --small: 0.8em;
-        --large: 1.2em;
-        --bold: 700;
-        --keyword-color: hsl(0, 0%, 30%);
-        --keyword-background: hsl(0, 0%, 92%);
-      }
-      body {
-        max-width: 800px;
-        font-family: 'Merriweather', sans-serif;
-        font-size: 16px;
-        font-weight: 300;
-        line-height: 1.4em;
-        margin: 0 auto;
-        padding: 5vh 10vw;
-        background: var(--background);
-        color: var(--text-color);
-      }
-      hr {
-        border: 0;
-        height: 0;
-        border-top: 1px solid rgba(0, 0, 0, 0.1);
-        border-bottom: 1px solid rgba(255, 255, 255, 0.3);
-        margin: 1.2em 0;
-      }
-      i {
-        margin-right: 0.4em;
-      }
-      h1 {
-        margin: 0.4em 0;
-        font-size: 3em;
-      }
-      h2 {
-        margin: 2em 0 1.2em;
-        font-size: 2em;
-      }
-      h1,
-      h2 {
-        color: var(--theme-color);
-        text-transform: uppercase;
-        font-weight: 200;
-        font-family: 'Exo 2', sans-serif;
-        line-height: 1.1em;
-      }
-      a {
-        text-decoration: none;
-        color: var(--anchor-color);
-      }
-      a:hover {
-        color: var(--theme-color);
-      }
-      p {
-        margin: 0.5em 0;
-      }
-      ul {
-        padding-left: 25px;
-        margin: 0.4em 0;
-        list-style-type: disc;
-      }
-      li {
-        margin: 0.2em 0;
-      }
-      section > ul {
-        list-style: none;
-        padding: 0;
-      }
-      section > ul > li {
-        margin-bottom: 2.5em;
-      }
-      .light {
-        color: var(--light-color);
-      }
-      .large {
-        font-size: var(--large);
-      }
-    `}</style>
-  </>
-);
+        :root {
+          --background: white;
+          --text-color: hsl(0, 0%, 15%);
+          --theme-color: hsl(26, 100%, 50%);
+          --light-color: hsl(0, 0%, 60%);
+          --anchor-color: hsl(26, 100%, 25%);
+          --small: 0.8em;
+          --large: 1.2em;
+          --bold: 700;
+          --keyword-color: hsl(0, 0%, 30%);
+          --keyword-background: hsl(0, 0%, 92%);
+        }
+        body {
+          max-width: 800px;
+          font-family: 'Merriweather', sans-serif;
+          font-size: 16px;
+          font-weight: 300;
+          line-height: 1.4em;
+          margin: 0 auto;
+          padding: 5vh 10vw;
+          background: var(--background);
+          color: var(--text-color);
+        }
+        hr {
+          border: 0;
+          height: 0;
+          border-top: 1px solid rgba(0, 0, 0, 0.1);
+          border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+          margin: 1.2em 0;
+        }
+        i {
+          margin-right: 0.4em;
+        }
+        h1 {
+          margin: 0.4em 0;
+          font-size: 3em;
+        }
+        h2 {
+          margin: 2em 0 1.2em;
+          font-size: 2em;
+        }
+        h1,
+        h2 {
+          color: var(--theme-color);
+          text-transform: uppercase;
+          font-weight: 200;
+          font-family: 'Exo 2', sans-serif;
+          line-height: 1.1em;
+        }
+        a {
+          text-decoration: none;
+          color: var(--anchor-color);
+        }
+        a:hover {
+          color: var(--theme-color);
+        }
+        p {
+          margin: 0.5em 0;
+        }
+        ul {
+          padding-left: 25px;
+          margin: 0.4em 0;
+          list-style-type: disc;
+        }
+        li {
+          margin: 0.2em 0;
+        }
+        section > ul {
+          list-style: none;
+          padding: 0;
+        }
+        section > ul > li {
+          margin-bottom: 2.5em;
+        }
+        .light {
+          color: var(--light-color);
+        }
+        .large {
+          font-size: var(--large);
+        }
+      `}</style>
+    </>
+  );
+};
+
+/* ~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^
+ * Localisation & Helpers
+ * ~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^ */
+
+const strings = new LocalizedStrings({
+  en: {
+    to: 'to',
+    since: 'Since',
+    work: 'Work History',
+    education: 'Education',
+    awards: 'Awards',
+    publications: 'Publications',
+    skills: 'Skills',
+    languages: 'Languages',
+  },
+  de: {
+    to: 'bis',
+    since: 'Seit',
+    work: 'Beruflicher Werdegang',
+    education: 'Ausbildung',
+    awards: 'Auszeichnungen',
+    publications: 'Publikationen',
+    skills: 'Erfahrung',
+    languages: 'Sprachen',
+  },
+});
 
 const Duration = ({ from, until }) => (
-  <div className="light">{until ? `${from} to ${until}` : `Since ${from}`}</div>
+  <div className="light">
+    {until ? `${from} ${strings.to} ${until}` : `${strings.since} ${from}`}
+  </div>
 );
 
 /* ~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^
@@ -204,7 +238,7 @@ export const sections = {
     <section>
       <h2>
         <i className="fa fa-suitcase" />
-        Work History
+        {strings.work}
       </h2>
       <ul>
         {data.map((props, i) => (
@@ -267,7 +301,7 @@ export const sections = {
     <section>
       <h2>
         <i className="fa fa-graduation-cap" />
-        Education
+        {strings.education}
       </h2>
       <ul>
         {data.map((props, i) => (
@@ -298,7 +332,7 @@ export const sections = {
     <section>
       <h2>
         <i className="fa fa-medal" />
-        Awards
+        {strings.awards}
       </h2>
       <ul>
         {data.map((props, i) => (
@@ -325,7 +359,7 @@ export const sections = {
     <section>
       <h2>
         <i className="fa fa-pen-fancy" />
-        Publications
+        {strings.publications}
       </h2>
       <ul>
         {data.map((props, i) => (
@@ -351,7 +385,7 @@ export const sections = {
     <section>
       <h2>
         <i className="fa fa-toolbox" />
-        Skills
+        {strings.skills}
       </h2>
       <ul>
         {data.map((props, i) => (
@@ -379,7 +413,7 @@ export const sections = {
     <section>
       <h2>
         <i className="fa fa-comments" />
-        Languages
+        {strings.languages}
       </h2>
       <ul>
         {data.map((props, i) => (
